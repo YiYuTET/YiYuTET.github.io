2100c16e8c9c341a8a6d84d6a2d9bfedc0128c4f
YiYuTET <2576556095@qq.com>
2024-02-29 17:22:04 +0800

    Site updated: 2024-02-29 17:22:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/da2f3447.html","post/4a17b156.html"];function toRandomPost(){
+var posts=["post/4a17b156.html","post/da2f3447.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
