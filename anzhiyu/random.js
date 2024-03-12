16b24c2a93b751bb74cc345ad6492015b91f1883
YiYuTET <2576556095@qq.com>
2024-03-12 09:59:25 +0800

    Site updated: 2024-03-12 09:59:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/4a17b156.html","post/da2f3447.html","post/139d3597.html"];function toRandomPost(){
+var posts=["post/8716a180.html","post/da2f3447.html","post/139d3597.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
