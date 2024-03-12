f89d264eb5ce2ecf160e53b22f99108d3516f1fa
YiYuTET <2576556095@qq.com>
2024-03-12 10:12:45 +0800

    Site updated: 2024-03-12 10:12:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/8716a180.html","post/da2f3447.html","post/139d3597.html"];function toRandomPost(){
+var posts=["post/58eeb88f.html","post/216d6a00.html","post/79d4ebb8.html","post/fa7bdefa.html","post/b2c5a78a.html","post/da2f3447.html","post/8716a180.html","post/139d3597.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
